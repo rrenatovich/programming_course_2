06400520ed43682edc171520036c743c702df7f4
skrool <206881066+skroolll@users.noreply.github.com>
2026-02-17 19:33:42 +0700

    косметический

--- lab8.cpp
+++ lab8.cpp
@@ -4,22 +4,22 @@ using namespace std;
 int main() {
     const int SIZE = 10;
 
-    cout << "\nСтатический массив + индексы" << endl;
+// 1 СПОСОБ
     int arr1[SIZE];
 
-    cout << "Результат: ";
+    cout << "Статический массив + индексы: ";
     for (int i = 0; i < SIZE; i++) {
         arr1[i] = i * i;
         cout << arr1[i] << " ";
     }
     cout << endl;
 
-    cout << "\nСтатический массив + указатели" << endl;
+// 2 СПОСОБ
     int arr2[SIZE];
 
     int* ptr2 = arr2;
 
-    cout << "Результат: ";
+    cout << "Статический массив + указатели: ";
     for (int i = 0; i < SIZE; i++) {
         *ptr2 = i * i;  
         cout << *ptr2 << " ";
@@ -28,10 +28,10 @@ int main() {
     ptr2 = arr2;
     cout << endl;
 
-    cout << "\nДинамический массив + индексы" << endl;
+// 3 СПОСОБ
     int* arr3 = new int[SIZE];
 
-    cout << "Результат: ";
+    cout << "Динамический массив + индексы: ";
     for (int i = 0; i < SIZE; i++) {
         arr3[i] = i * i;
         cout << arr3[i] << " ";
@@ -39,13 +39,13 @@ int main() {
     delete[] arr3;
     cout << endl;
 
-    cout << "\nДинамический массив + указатели" << endl;
+// 4 СПОСОБ
     int* arr4 = new int[SIZE];
 
     int* ptr4 = arr4;
     int* begin4 = arr4;
 
-    cout << "Результат: ";
+    cout << "Динамический массив + указатели: ";
     for (int i = 0; i < SIZE; i++) {
         *ptr4 = i * i;
         cout << *ptr4 << " ";
