7db6c1750649d97d71e27351c8edc18e279fbf85
skrool <206881066+skroolll@users.noreply.github.com>
2026-02-17 19:29:14 +0700

    4 способ

--- lab8.cpp
+++ lab8.cpp
@@ -7,18 +7,12 @@ int main() {
     cout << "\nСтатический массив + индексы" << endl;
     int arr1[SIZE];
 
-    for (int i = 0; i < SIZE; i++) {
-        arr1[i] = i * i;  
-    }
-
     cout << "Результат: ";
     for (int i = 0; i < SIZE; i++) {
+        arr1[i] = i * i;
         cout << arr1[i] << " ";
     }
     cout << endl;
-    
-
-
 
     cout << "\nСтатический массив + указатели" << endl;
     int arr2[SIZE];
@@ -34,21 +28,32 @@ int main() {
     ptr2 = arr2;
     cout << endl;
 
-
-
-
     cout << "\nДинамический массив + индексы" << endl;
     int* arr3 = new int[SIZE];
 
+    cout << "Результат: ";
     for (int i = 0; i < SIZE; i++) {
         arr3[i] = i * i;
+        cout << arr3[i] << " ";
     }
+    delete[] arr3;
+    cout << endl;
+
+    cout << "\nДинамический массив + указатели" << endl;
+    int* arr4 = new int[SIZE];
+
+    int* ptr4 = arr4;
+    int* begin4 = arr4;
 
     cout << "Результат: ";
     for (int i = 0; i < SIZE; i++) {
-        cout << arr3[i] << " ";
+        *ptr4 = i * i;
+        cout << *ptr4 << " ";
+        ptr4++;
     }
+    
+    ptr4 = begin4;
     cout << endl;
 
-    delete[] arr3;
+    
 }
