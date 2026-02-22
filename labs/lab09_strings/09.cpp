1c54c8959b821d3782e6298c216d6b437b6950e3
skrool <206881066+skroolll@users.noreply.github.com.>
2026-02-22 23:22:54 +0700

    1 задание string

--- labs/lab09_strings/09.cpp
+++ labs/lab09_strings/09.cpp
@@ -7,7 +7,7 @@ void demchar() {
     cout << "char" << endl;
     
     // длина
-    char str1[] = "Hello";
+    char str1[] = "hello";
     cout << "1. strlen: " << strlen(str1) << endl;
     
     // копирование
@@ -16,11 +16,30 @@ void demchar() {
     cout << "2. strcpy: " << str2 << endl;
 
     // соединение
-    strcat(str2, " World");
+    strcat(str2, " world");
     cout << "3. strcat: " << str2 << endl;
+
+}
+
+
+void demstring() {
+    cout << "\nstring" << endl;
+
+    //длина
+    string s1 = "hello";
+    cout << "1. lenght: " << s1.length() << endl;
+
+    //добавление
+    s1.append(" world");
+    cout << "2. append: " << s1 << endl;
+
+    //извлечение
+    string s2 = s1.substr(0,5);
+    cout << "3. substr: " << s2 << endl;
 
 }
 
 int main() {
     demchar();
+    demstring();
 }
